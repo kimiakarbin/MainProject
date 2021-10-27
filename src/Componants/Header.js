23452d022402792e8c83db5b7dd585b4ea0d95aa
kimiakarbin <kimiakarbin@gmail.com>
2021-10-27 16:08:38 +0330

    build  header

---
+++ src/Componants/Header.js
@@ -0,0 +1,111 @@
+import react from 'react';
+import { useState } from 'react';
+
+const Header = () => {
+    return (
+        <div className="App">
+            <div className="For Buyer">
+                <span>For Buyer</span>
+                <ul>
+                    <li>
+                        <dl>
+                            <dt>Search Products & Suppliers</dt>
+                            <dd>Product Directory</dd>
+                            <dd>Supplier Discovery</dd>
+                            <dd>Video Channel</dd>
+                            <dd>Post Sourcing Request</dd>
+                        </dl>
+                        <dl>
+                            <dt>Sourcing Solutions</dt>
+                            <dd>Source From Industry Hubs</dd>
+                            <dd>Customize Your Products</dd>
+                            <dd> MEI Awards-Winning Products</dd>
+                            <dd>Smart Expo</dd>
+                        </dl>
+                    </li>
+                    <li>
+                        <dl>
+                            <dt>Service</dt>
+                            <dd>New User Guide</dd>
+                            <dd>Star Buyer</dd>
+                            <dd>Product Alert</dd>
+                            <dd>Supplier Audit Report</dd>
+                            <dd>Meet Suppliers</dd>
+                            <dd>Contact Us</dd>
+                        </dl>
+                        <dl>
+                            <dt>Quick Links</dt>
+                            <dd>My Favorites</dd>
+                            <dd>Recent Visit History</dd>
+                        </dl>
+                    </li>
+                </ul>
+            </div>
+            <br />
+            <div className="For Supplier">
+                <span>For Supplier</span>
+                <ul>
+                    <li>1</li>
+                    <li>2</li>
+                    <li>3</li>
+                    <li>4</li>
+                    <li>Search Sourcing Requests </li>
+                    <li>Import & Export Service</li>
+                    <li>7</li>
+                    <li>
+                        <button>button 1</button>
+                        <button>button 2</button>
+                    </li>
+                </ul>
+            </div>
+            <br />
+            <div className="Get Apps">
+                <span>Get Apps</span>
+                <div>Download App!</div>
+                <div>Explore App Exclusive Discounts</div>
+            </div>
+            <br />
+            <div className="English">
+                <span>English</span>
+                <ul>
+                    <li>lng 1</li>
+                    <li>lng 2</li>
+                    <li>lng 3</li>
+                    <li>lng 4</li>
+                    <li>lng 5</li>
+                    <li>lng 6</li>
+                    <li>lng 7</li>
+                    <li>lng 8</li>
+                    <li>lng 9</li>
+                    <li>lng 10</li>
+                </ul>
+            </div>
+            <div>
+                <h1>Made-in-china</h1>
+                <form>
+                    <div className="search tabs">
+                        <div>Products</div>
+                        <div>Suppliers</div>
+                        <div>Sourcing Requests</div>
+                    </div>
+                    <div>
+                        <input type="text" name="word" />
+                    </div>
+                    <div className="serch--btn">
+                        <button>Search</button>
+                    </div>
+                </form>
+                <div className="sign--box">
+                    <a href="bwxj">Sign In</a>
+                    <br />
+                    <a href="ewdwxj">Join Free</a>
+                </div>
+                <div className="basket--text">
+                    <span>Inquiry Basket</span>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
